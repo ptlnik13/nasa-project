9a63c2dd604a45e473e56c0dd6881bf93bd33344
ptlnik13 <nikeshvpatel@gmail.com>
2022-11-27 15:35:55 -0500

    addinv /v1 to server and client

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require("path");
 const cors = require('cors');
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require("./routes/launches/launches.router");
+const {api} = require('./routes/api');
 const morgan = require("morgan");
 
 const app = express();
@@ -13,8 +12,8 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
