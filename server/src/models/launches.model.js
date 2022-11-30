40e8cafcecc50b0cba87ed1c92c7eb10149dc5e6
ptlnik13 <nikeshvpatel@gmail.com>
2022-11-30 17:10:27 -0500

    adding pagination into options to get all the data in once

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -22,6 +22,7 @@ async function loadLaunchData() {
     const response =await axios.post(SPACEX_API_URL, {
         query: {},
         options: {
+            pagination:false,
             populate: [{
                 path: 'rocket',
                 select: {name: 1}
